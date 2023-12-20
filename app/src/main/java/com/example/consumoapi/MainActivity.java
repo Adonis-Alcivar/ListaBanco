fbd2a1b9b66c85abb615ee383ca74c04a25bee7a
UTEQ <aalcivarn@uteq.edu.ec>
2023-12-20 11:01:11 -0500

    ACTIVITY2

--- app/src/main/java/com/example/consumoapi/MainActivity.java
+++ app/src/main/java/com/example/consumoapi/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.consumoapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -20,13 +22,26 @@ public class MainActivity extends AppCompatActivity implements Asynchtask {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+    public void Enviar(View view){
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        Bundle b = new Bundle();
+        intent.putExtras(b);
+        starActivity(intent);
+    }
+
+    private void starActivity(Intent intent) {
+    }
+
     public void clickLogin(View v){
         Bundle bundle = this.getIntent().getExtras();
         Map<String, String> datos = new HashMap<String, String>();
+        EditText txtUsuario= findViewById(R.id.txtusuario);
+        EditText txtClaves= findViewById(R.id.txtclave);
         WebService ws= new WebService(
-                "https://revistas.uteq.edu.ec/ws/login.php?usr=cristian&pass=123"
-                        + bundle.getString("Usr") + "&pass=" + bundle.getString("clave"),
+                "https://revistas.uteq.edu.ec/ws/login.php?usr=" + txtUsuario.getText().toString() +
+                "&pass=" + txtClaves.getText().toString(),
                 datos,
                 MainActivity.this, MainActivity.this);
         ws.execute("GET");
